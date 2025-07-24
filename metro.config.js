56f15172a8de8d88caab4c0a311fd35525da63ab
GHwyever <wyever@gmail.com>
2025-07-24 15:40:41 +0800

    修复 Babel 和 Metro 配置

--- metro.config.js
+++ metro.config.js
@@ -1,22 +1,7 @@
-   // metro.config.js
-   const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require('expo/metro-config');
 
-   const config = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname);
 
-   // 添加 TypeScript 支持
-   config.resolver.sourceExts = [
-     'js',
-     'jsx',
-     'ts',
-     'tsx',
-     'json',
-     'cjs' // 如果需要支持 CommonJS
-   ];
+config.resolver.sourceExts = ['js', 'jsx', 'ts', 'tsx', 'json'];
 
-   // 添加 asset 支持
-   config.resolver.assetExts = [
-     ...config.resolver.assetExts.filter(ext => ext !== 'svg'),
-     'bin'
-   ];
-
-   module.exports = config;
+module.exports = config;
