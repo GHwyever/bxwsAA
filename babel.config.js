a21f4817ccf94065e69195d7c92ad89e6e0e820b
GHwyever <wyever@gmail.com>
2025-07-24 17:02:43 +0800

    直接修复 TypeScript 扩展名问题

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,6 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: [
-      'babel-preset-expo',
-      '@babel/preset-typescript'
-    ],
-    plugins: [
-      '@babel/plugin-proposal-export-namespace-from',
-      '@babel/plugin-transform-typescript',
-      'react-native-reanimated/plugin'
-    ]
+    presets: ['babel-preset-expo'],
   };
 };
--- metro.config.js
+++ metro.config.js
@@ -2,17 +2,4 @@ const { getDefaultConfig } = require('@expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// 关键：添加TypeScript扩展名到sourceExts
-config.resolver.sourceExts = [
-  ...config.resolver.sourceExts,
-  'ts',
-  'tsx'
-];
-
-// 可选：如果有SVG或其他资源，添加assetExts
-config.resolver.assetExts = [
-  ...config.resolver.assetExts,
-  'svg'
-];
-
 module.exports = config;
